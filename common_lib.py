91bf689b4bd4a9181c665a2c0257e9a1601b22e4
Luiz <luizfeliperomero10@gmail.com>
2023-05-12 23:13:07 -0300

    Add new layout and style to html

--- common_lib.py
+++ common_lib.py
@@ -18,7 +18,7 @@ def get_html():
 
 def generate_html(books):
     html_file = open("index.html", "w+")
-    html_file.write("<!DOCTYPE html><html lang='pt-br'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Books Scrapping - Luiz Felipe</title><link rel='stylesheet' href='./styles.css'><link rel='icon' href='./favicon.ico' type='image/x-icon'></head><body><main class='content'><h1>Variação de Preços</h1><h2>Livros de 3 a 5 estrelas do site <a href='https://books.toscrape.com'>https://books.toscrape.com</a></h2><div class='books'>")
+    html_file.write("<!DOCTYPE html><html lang='pt-br'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Books Scrapping - Luiz Felipe</title><link rel='stylesheet' href='./styles.css'><link rel='icon' href='./favicon.ico' type='image/x-icon'></head><body><main class='content'><div class='info'><h1>Variação de Preços</h1><h2>Livros de 3 a 5 estrelas do site <a href='https://books.toscrape.com'>https://books.toscrape.com</a></h2></div><div class='books'>")
 
     for book in books:
         html_file.write("<div class='book'><p class='name'>" + book.name + "</p><p class='prices'>" + Currency.EUR.value + str(book.first_price) + " " + u"\u2192" + " " + Currency.EUR.value + str(book.last_price) + "</p><p class='variation'>" + str(book.get_price_variation()) + "</p></div>");
