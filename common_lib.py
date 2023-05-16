afc41b1af7ea1b7f73fe6a8c69b20a5494dd88a3
Luiz <luizfeliperomero10@gmail.com>
2023-05-16 18:05:29 -0300

    Show results even if Api Key is invalid or not provided

--- common_lib.py
+++ common_lib.py
@@ -9,7 +9,7 @@ def get_api_key():
     key = "";
     if(os.getenv("OPENAI_API_KEY") != None):
         key = os.getenv("OPENAI_API_KEY")
-    else:
+    elif(len(sys.argv) > 1):
         key = sys.argv[1]
     return key
 
@@ -31,9 +31,16 @@ def get_html():
         raise Exception(http_error)
 
 def generate_html(books):
-    chat_gpt_analysis = get_chat_gpt_analysis()
+    html = "<!DOCTYPE html><html lang='pt-br'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Books Scrapping - Luiz Felipe</title><link rel='stylesheet' href='./styles.css'><link rel='icon' href='./favicon.ico' type='image/x-icon'></head><body><div class='top_bar'><h1>Variação de Preços</h1></div><main class='content'><div class='info'><h2>Livros de 3 a 5 estrelas do site <a href='https://books.toscrape.com'>https://books.toscrape.com</a></h2></div><div class='books'>"
+    try:
+        chat_gpt_analysis = get_chat_gpt_analysis()
+        html_analysis = "<div class='analysis'><h3>Análise do ChatGPT</h3><article class='analysis-text'>" + chat_gpt_analysis + "</article></div>"
+        html = "<!DOCTYPE html><html lang='pt-br'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Books Scrapping - Luiz Felipe</title><link rel='stylesheet' href='./styles.css'><link rel='icon' href='./favicon.ico' type='image/x-icon'></head><body><div class='top_bar'><h1>Variação de Preços</h1></div><main class='content'><div class='info'><h2>Livros de 3 a 5 estrelas do site <a href='https://books.toscrape.com'>https://books.toscrape.com</a></h2>" +  html_analysis + "</div><div class='books'>"
+    except Exception as e:
+        print("Análise do ChatGPT não será gerada pois a API Key fornecida é inválida")
+
     html_file = open(os.path.join(OUTPUT_FILES_DIR, "index.html"), "w+")
-    html_file.write("<!DOCTYPE html><html lang='pt-br'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Books Scrapping - Luiz Felipe</title><link rel='stylesheet' href='./styles.css'><link rel='icon' href='./favicon.ico' type='image/x-icon'></head><body><div class='top_bar'><h1>Variação de Preços</h1></div><main class='content'><div class='info'><h2>Livros de 3 a 5 estrelas do site <a href='https://books.toscrape.com'>https://books.toscrape.com</a></h2><div class='analysis'><h3>Análise do ChatGPT</h3><article class='analysis-text'>" + chat_gpt_analysis + "</article></div></div><div class='books'>")
+    html_file.write(html)
 
     for book in books:
         html_file.write("<div class='book'><p class='name'>" + book.name + "</p><p class='prices'>" + Currency.EUR.value + str(book.first_price) + " " + u"\u2192" + " " + Currency.EUR.value + str(book.last_price) + "</p><p class='variation'>" + str(book.get_price_variation()) + "</p></div>");
